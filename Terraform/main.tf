da2c3ab7f13a7a675e7190ab5024ef73fb9f11f3
Brijesh Prajapati <130594666+brijeshprajapati53@users.noreply.github.com>
2025-04-26 11:34:03 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 
+
 resource "azurerm_storage_account" "exam_sa" {
   name                     = "storagebrijesh678"
   resource_group_name      = azurerm_resource_group.main.name
