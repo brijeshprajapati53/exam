a6cd13fa189ffe050e851df6cc94ec4e7fd4f84e
Brijesh Prajapati <130594666+brijeshprajapati53@users.noreply.github.com>
2025-04-26 11:34:45 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "exam_sa" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acrbrijesh123"
+  name                = "acrbrijesh1238"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku                 = "Basic"
